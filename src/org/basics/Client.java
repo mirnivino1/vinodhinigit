679f7298454a7d211fe7993b73f816533f420b3b
sathyaprabha26 <erprabha.sathya@gmail.com>
2021-12-24 09:22:49 +0530

    second branch

--- src/org/basics/Client.java
+++ src/org/basics/Client.java
@@ -5,7 +5,10 @@ public class Client  {
 	public void cliID() {
 		System.out.println("321");
 	}
-	
+	public void clisalary() {
+
+		System.out.println("expected");
+	}
 	public void cliName() {
 		System.out.println("Greens");
 	}
